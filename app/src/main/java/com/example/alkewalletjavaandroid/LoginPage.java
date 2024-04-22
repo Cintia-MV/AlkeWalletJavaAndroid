ab55cf9f1118031d040866b45aef9f6041dbbaa2
Cintia MV <cintiamuva@gmail.com>
2024-04-22 02:38:21 -0400

    Quinta pantalla lista

--- app/src/main/java/com/example/alkewalletjavaandroid/LoginPage.java
+++ app/src/main/java/com/example/alkewalletjavaandroid/LoginPage.java
@@ -3,6 +3,7 @@ package com.example.alkewalletjavaandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class LoginPage extends AppCompatActivity {
 
     TextView crearCuenta;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +30,15 @@ public class LoginPage extends AppCompatActivity {
                 startActivity(crearCuenta);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(LoginPage.this, HomePage.class);
+                startActivity(login);
+            }
+        });
+
+
     }
 }
